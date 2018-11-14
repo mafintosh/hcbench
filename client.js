49f897d6b99d6d63a1960f213657ce01657e1f3e
Emil Bay <github@tixz.dk>
2018-11-14 12:42:23 +0100

    init

---
+++ client.js
@@ -0,0 +1,42 @@
+const noise = require('../..')
+const network = require('@hyperswarm/network')
+const jsonStream = require('duplex-json-stream')
+const hypercore = require('hypercore')
+const pump = require('pump')
+
+const swarm = network()
+const hyperswarm = network()
+
+const publicKey = Buffer.from(process.argv[2], 'hex')
+const feeds = new Map()
+const client = noise.keygen()
+
+swarm.on('connection', function (stream) {
+  const encryptedStream = noise(stream, true, {
+    pattern: 'XK',
+    staticKeyPair: client,
+    remoteStaticKey: publicKey
+  })
+
+  encryptedStream.on('error', (err) => {
+    console.error(err)
+  })
+
+  encryptedStream.write(Buffer.from('my-id'))
+
+  encryptedStream.once('readable', function () {
+    var hello = JSON.parse(encryptedStream.read().toString())
+    if (hello.error) {
+      console.log(hello.message)
+      process.exit
+    }
+    const feed = hypercore(`./local/test`, key)
+
+    pump(encryptedStream, feed.replicate({ live: true }), encryptedStream)
+  })
+})
+
+swarm.join(publicKey, {
+  announce: false,
+  lookup: true
+})
---
+++ server.js
@@ -0,0 +1,69 @@
+const noise = require('../..')
+const network = require('@hyperswarm/network')
+const jsonStream = require('duplex-json-stream')
+const hypercore = require('hypercore')
+const protocol = require('hypercore-protocol')
+const pump = require('pump')
+
+const server = noise.seedKeygen(Buffer.alloc(32, 'secret'))
+
+console.log(server.publicKey.toString('hex'))
+
+const swarm = network()
+const hyperswarm = network()
+const feeds = new Map()
+
+swarm.on('connection', function (stream) {
+  const encryptedStream = noise(stream, false, {
+    pattern: 'XK',
+    staticKeyPair: server,
+    onstatickey: function (remoteKey, done) {
+      console.log('new client key:', remoteKey)
+      done()
+    }
+  })
+
+  encryptedStream.on('error', (err) => {
+    console.error(err)
+  })
+
+  encryptedStream.once('readable', function () {
+    var id = encryptedStream.read()
+
+    if (!id.equals(Buffer.from('my-id'))) {
+      encryptedStream.end(JSON.stringify({
+        error: true,
+        message: 'Auth failed. Goodbye'
+      }))
+
+      return
+    }
+
+    console.log(id)
+    // do something with id
+
+    const feed = hypercore(`./remote/test`)
+    feed.ready((err) => {
+      if (encryptedStream.destroyed) return
+      if (err) return encryptedStream.destroy(err)
+
+      encryptedStream.write(JSON.stringify({
+        key: feed.key.toString('hex')
+      }))
+
+      pump(encryptedStream, feed.replicate({ live: true }), encryptedStream, function (err) {
+        console.log('replication ended', err)
+      })
+
+      const key = feed.key.toString('hex')
+      console.log(key)
+
+      feed.append('important history data')
+    })
+  })
+})
+
+swarm.join(server.publicKey, {
+  announce: true,
+  lookup: false
+})
